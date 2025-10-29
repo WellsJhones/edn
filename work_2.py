46add7390ce5940669e96800d5477c472ab390a8
WellsJhones <wellsjhones@gmail.com>
2025-10-29 10:47:46 -0300

    atividade 1

---
+++ work_2.py
@@ -0,0 +1,8 @@
+print("Welcome to the Py Calculator")
+
+print("You'll now have to enter 2 numbers, so be ready!")
+first_number = int(input("Type the first one: \n"))
+second_number = int(input("Type now the second one: \n"))
+
+# Convert result to string before concatenating
+print("Result: " + str(first_number + second_number))
